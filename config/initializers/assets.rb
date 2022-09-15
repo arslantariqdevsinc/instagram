e3051b3dd15a517c1bd3e708fe060b77a83d94db
arslan tariq <arslant374@gmail.com>
2022-09-16 00:05:01 +0500

    configure scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w[devise.css]
+Rails.application.config.assets.precompile += %w[devise.scss]
 Rails.application.config.assets.precompile += %w[devise.js]
 Rails.application.config.assets.precompile += %w[application.scss]
 
